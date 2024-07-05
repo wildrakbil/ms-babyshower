87fbb80ee7247b899571af91199c8f948ed91d8f
william Garzon <william.garzon@globant.com>
2024-07-04 19:42:18 -0500

    fix: ajuste a la concurrecia del json

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const cors = require('cors');
+const lockfile = require('lockfile');
 
 const app = express();
 const PORT = process.env.PORT || 8088;
@@ -31,41 +32,61 @@ app.get('/ms-event-producer/gift', (req, res) => {
 
 // Ruta PUT para actualizar un regalo por su ID
 app.put('/ms-event-producer/gift/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, isEdit } = req.body;
-  
+  const { id } = req.params;
+  const { name, isEdit } = req.body;
+
+  lockfile.lock(DATA_FILE, { retries: 10 }, (err) => {
+    if (err) {
+      console.error('Error locking file:', err);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+
     fs.readFile(DATA_FILE, 'utf8', (err, data) => {
       if (err) {
         console.error('Error reading file:', err);
+        lockfile.unlock(DATA_FILE, err => {
+          if (err) console.error('Error unlocking file:', err);
+        });
         res.status(500).json({ error: 'Internal server error' });
         return;
       }
-  
+
       let gifts = JSON.parse(data);
       const index = gifts.findIndex(gift => gift.id === parseInt(id));
       if (index === -1) {
+        lockfile.unlock(DATA_FILE, err => {
+          if (err) console.error('Error unlocking file:', err);
+        });
         res.status(404).json({ error: 'Gift not found' });
         return;
       }
-  
+
       // Actualiza el nombre y el estado de edición del regalo
       gifts[index].name = name;
       gifts[index].isEdit = isEdit;
-  
+
       // Escribe los cambios en el archivo JSON
       fs.writeFile(DATA_FILE, JSON.stringify(gifts, null, 2), 'utf8', err => {
         if (err) {
           console.error('Error writing file:', err);
+          lockfile.unlock(DATA_FILE, err => {
+            if (err) console.error('Error unlocking file:', err);
+          });
           res.status(500).json({ error: 'Internal server error' });
           return;
         }
-  
+
+        lockfile.unlock(DATA_FILE, err => {
+          if (err) console.error('Error unlocking file:', err);
+        });
+
         // Retorna el regalo actualizado como respuesta
         res.json(gifts[index]);
       });
     });
   });
-  
+});
 
 // Configuración para escuchar el puerto
 app.listen(PORT, () => {
